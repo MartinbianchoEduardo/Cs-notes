b928d57babd89447d43fe7b1a6caebb8db904ade
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-05-03 14:51:34 -0300

    first commit

---
+++ C#-2/C#-Intermediate/Customer.cs
@@ -0,0 +1,29 @@
+﻿namespace Lectures
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        //the readonly keyword makes sure that only one instance of this will be made
+        //if we try to initialize another List of orders, we'll get an error in the IDE
+        public readonly List<Order> Orders = new List<Order>();
+        //whenever you have a class, with a list attribute like the one above
+        //always initialize that list as an empty list 
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id) //delegate the control to the other constructor above
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+
+        }
+    }
+}
---
+++ C#-2/C#-Intermediate/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace Lectures
+{
+    public class Order
+    {
+
+    }
+}
