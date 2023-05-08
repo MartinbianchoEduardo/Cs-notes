1fad4a04f69e3a50ded796ab3e36692066ee4103
MartinbianchoEduardo <martinbianchoeduardo@gmail.com>
2023-05-08 14:07:02 -0300

    add async code

---
+++ advanced/C#-AdvancedTopics/AsyncProgramming.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace C__AdvancedTopics
+{
+    internal class AsyncProgramming
+    {
+        //public async 
+        //the return type is a Task - an object that encapsulates the
+        //state of an async operation 
+        //for void methods = Task
+        //for non-void methods - use generic = Task< return Type >
+        public async Task DownloadAsync(string url)
+        {
+            var webClient = new WebClient();
+            //this method returns a Task, so await it
+            var html = await webClient.DownloadStringTaskAsync(url);
+
+            using (var streamWriter = new StreamWriter(@"c:/projects/result.html"))
+            {
+                await streamWriter.WriteAsync(html);
+            }
+        }
+    }
+}
