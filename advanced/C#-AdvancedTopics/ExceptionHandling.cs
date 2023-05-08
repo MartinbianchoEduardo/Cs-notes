1cd21455b952a10cff1ed546b063b877ad47d19d
MartinbianchoEduardo <martinbianchoeduardo@gmail.com>
2023-05-08 13:29:37 -0300

    add exception handling

---
+++ advanced/C#-AdvancedTopics/ExceptionHandling.cs
@@ -0,0 +1,38 @@
+﻿namespace C__AdvancedTopics
+{
+    internal class ExceptionHandling
+    {
+
+        static void main(string[] args)
+        {
+            var Calculator = new Calculator();
+
+            try
+            {
+            Console.WriteLine(Calculator.Divide(5, 0));
+
+            } catch (Exception ex)
+            {
+                throw new CalculatorException("calculator went wrong", ex);
+            }
+        }
+
+    }
+
+    //custom exception creation
+    public class CalculatorException : Exception { 
+    public CalculatorException(string message, Exception InnerException) 
+            : base(message, InnerException)
+        {}
+    }
+
+    public class Calculator
+    {
+        public Calculator() { }
+
+        public float Divide(int num, int den)
+        {
+            return num / den;
+        }
+    }
+}
