d1aa6e157687c3cf88104b81b7d92714879786c4
MartinbianchoEduardo <martinbianchoeduardo@gmail.com>
2023-05-08 10:31:37 -0300

    add lambda expressions

---
+++ advanced/C#-AdvancedTopics/LambdaExpessions.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace C__AdvancedTopics
+{
+
+    //Lambda Expressions are anonymous functions 
+    //used for convenience
+
+    //syntax: args => expression
+    //"args 'goes to' expression"
+    class LambdaExpessions
+    {
+        static void main(String[] args) {
+
+            //use delegates
+            //Func<argument , return type>
+
+            Func<int, int> square = Square;
+
+            //both of these are the same 
+
+            Func<int, int> square = n => n * n;
+
+            Console.WriteLine(square(5));
+        }
+
+        static int Square(int n)
+        {
+            return n * n;
+        }
+    }
+}
